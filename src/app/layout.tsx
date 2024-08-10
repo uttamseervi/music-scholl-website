99c662858c5e04981f643d453151a79192890f6a
= <=>
2024-08-10 12:07:06 +0530

    done with the home section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      
+    <html lang="en" className="dark">
+
       <body className={inter.className}>
-        <h1>Navbar</h1>
-        {children}</body>
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-
+import HeroSection from "@/components/HeroSection"
 export default function Home() {
   return (
-    <h1 className="text-center mt-20 text-3xl">
-      This is first tut for the nextJS
-    </h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <div className="">
+    <HeroSection/>
+    </div>
+    </main>
 
-  );
+);
 }
+
+/* CSS NOTES: bg-black/30 after slash opacity is given for more precise value we use [] brackets bg-black/[0.5]  */
