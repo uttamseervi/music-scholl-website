6d5bbcb2f57de86186e9e7da713091c7ff4392d8
= <=>
2024-08-11 17:39:05 +0530

    completed the testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import { CardSpotlightDemo } from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection"
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    <div className="">
-    <HeroSection/>
-    </div>
+      <div className="">
+        <HeroSection />
+        <CardSpotlightDemo />
+        <WhyChooseUs />
+        <MusicSchoolTestimonials />
+      </div>
     </main>
 
-);
+  );
 }
 
 /* CSS NOTES: bg-black/30 after slash opacity is given for more precise value we use [] brackets bg-black/[0.5]  */
